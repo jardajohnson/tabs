bedb91515c8dd853a02408df0aa524ad4aab0c1a
Aaron Johnson <sugarwaterpurple.js@gmail.com>
2022-07-11 14:34:21 -0700

    update tab component with logic to handle changing tabs

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -1,6 +1,28 @@
-const Tab = (props) => {
-	
-	return (  );
-}
- 
+import './Tab.css';
+
+const Tab = ({ tabs, setIndex, setContent }) => {
+	const handleClick = (e, ind) => {
+		setContent(e.value);
+		setIndex(ind + 1);
+	};
+
+	return (
+		<>
+			{tabs.map((tab, index) => {
+				return (
+					<button
+						key={index}
+						type='button'
+						onClick={(e) => handleClick(e.target, index)}
+						value={tab}
+						className='tab'
+					>
+						Tab {index + 1}
+					</button>
+				);
+			})}
+		</>
+	);
+};
+
 export default Tab;

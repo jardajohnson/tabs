9ea102c3437db5fbfb8b5cfed25b02501946c647
Aaron Johnson <sugarwaterpurple.js@gmail.com>
2022-07-11 14:35:14 -0700

    add components to app root

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { useState } from 'react';
 import TabArea from './components/TabArea';
+import Tab from './components/Tab';
+import './App.css';
 
 function App() {
+	const [tabs, setTabs] = useState(['hello']);
+	const [index, setIndex] = useState('--');
+	const [content, setContent] = useState('');
 
-	const [tabs, setTabs] = useState([]);
+	// const changeIndex = (index) => {
+
+	// }
 
 	return (
 		<div>
-			<header>
-				<TabArea />
+			<header className='container'>
+				<Tab tabs={tabs} setIndex={setIndex} setContent={setContent} />
+				<TabArea index={index} content={content} />
 			</header>
 		</div>
 	);
